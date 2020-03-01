69343f71d99c9af47a195e8e063ce81d0d8833e1
Zuzki <ldegrac071@hotmail.com>
2020-03-01 07:32:25 -0500

    Build Controls added and styled, don't have functionality

---
+++ src/components/Burger/Build_Controls/Build_Controls.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from './Build_Controls.css'
+
+const buildControls = (props) => (
+	<div className = { classes.BuildControls }>
+
+	</div>
+)
+
+export default buildControls
