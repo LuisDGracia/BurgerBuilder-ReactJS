6a1085d49ece84570bc92ff4d458846b24990e23
Zuzki <ldegrac071@hotmail.com>
2020-03-02 08:16:35 -0500

    Added price display and a check out button, not working atm

--- src/containers/Burger_Builder/Burger_Builder.js
+++ src/containers/Burger_Builder/Burger_Builder.js
@@ -6,9 +6,9 @@ import BuildControls from '../../components/Burger/Build_Controls/Build_Controls
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
+	bacon: 0.7,
 	cheese: 0.4,
-	meat: 1.3,
-	bacon: 0.7
+	meat: 1.3
 }
 class BurgerBuilder extends Component{
 
@@ -19,12 +19,26 @@ class BurgerBuilder extends Component{
 			cheese:0,
 			meat:  0
 		},
-		totalPrice: 4
+		totalPrice: 4,
+		purchasable: false
+	}
+
+	isPurchasable( ingredients ){
+		
+		const sum = Object.keys(ingredients)
+											.map( igKey => {
+												return ingredients[ igKey ]
+											})
+											.reduce( (sum, el) => {
+												return sum + el;
+											}, 0 );
+
+		this.setState({ purchasable: sum > 0 })
 	}
 
 	addIngredientHandler = ( ingredient ) => {
 		
-		const oldCount = this.state.ingredients[ingredient];
+		const oldCount = this.state.ingredients[ ingredient ];
 		const updatedCount = oldCount + 1;
 
 		const updatedIngredients = { ...this.state.ingredients }
@@ -35,25 +49,27 @@ class BurgerBuilder extends Component{
 
 		const newPrice = oldPrice + priceAddition;
 		this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+		this.isPurchasable( updatedIngredients);
 
 	};
 
 	removeIngredientHandler = ( ingredient ) => {
 		
-		const oldCount = this.state.ingredients[ingredient]
-		if( oldCount <= 0){
+		const oldCount = this.state.ingredients[ ingredient ]
+		if( oldCount <= 0 ){
 			return;
 		}
 
 		const updatedCount = oldCount - 1;
 		const updatedIngredients = { ...this.state.ingredients }
-		updatedIngredients[ingredient] = updatedCount
+		updatedIngredients[ ingredient ] = updatedCount
 
 		const priceDeduction = INGREDIENT_PRICES[ ingredient ];
 		const oldPrice = this.state.totalPrice;
 
 		const newPrice = oldPrice - priceDeduction;
 		this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+		this.isPurchasable( updatedIngredients );
 
 	}
 
@@ -72,7 +88,9 @@ class BurgerBuilder extends Component{
 				<BuildControls 
 					ingredientAdded = { this.addIngredientHandler } 
 					ingredientRemove = { this.removeIngredientHandler } 
-					disabled = {disabledInfo} />
+					disabled = { disabledInfo } 
+					purchasable = { this.state.purchasable }
+					price = { this.state.totalPrice }/>
 			</Aux>
 		)
 	}
