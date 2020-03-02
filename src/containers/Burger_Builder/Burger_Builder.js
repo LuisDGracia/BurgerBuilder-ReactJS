ec9bdabdfc9c2c9ab26d19ef189c8f933e714151
Zuzki <ldegrac071@hotmail.com>
2020-03-02 09:16:32 -0500

    Added functionalities to make information display dynamic

--- src/containers/Burger_Builder/Burger_Builder.js
+++ src/containers/Burger_Builder/Burger_Builder.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Aux from '../../hoc/Auxiliar'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/Build_Controls/Build_Controls'
+import Modal from '../../components/UI/Modal/Modal'
+import OrderSummary from '../../components/Burger/Order_Summary/Order_Summary'
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -84,6 +86,9 @@ class BurgerBuilder extends Component{
 
 		return (
 			<Aux>
+				<Modal >
+					<OrderSummary ingredients = { this.state.ingredients }/>
+				</Modal>
 				<Burger ingredients = { this.state.ingredients } />
 				<BuildControls 
 					ingredientAdded = { this.addIngredientHandler } 
