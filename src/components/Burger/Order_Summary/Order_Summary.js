ae7022e704e8aaf1fe7cf19ecf9758ea3bff4779
Zuzki <ldegrac071@hotmail.com>
2020-03-03 10:33:24 -0500

    Added Cancel and Continue btn to the modal with style

--- src/components/Burger/Order_Summary/Order_Summary.js
+++ src/components/Burger/Order_Summary/Order_Summary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Aux from '../../../hoc/Auxiliar'
+import Button from '../../UI/Button/Button'
 
 const orderSummary = ( props ) => {
 	
@@ -22,6 +23,8 @@ const orderSummary = ( props ) => {
 			</ul>
 			
 			<p> Continue to CheckOut?</p>
+			<Button btnType = "Danger" clicked = { props.purchaseCancelled } >CANCEL</Button>
+			<Button btnType = "Success" clicked = { props.purchaseContinued } >CONTINUE</Button>
 		</Aux>
 	)
 }
