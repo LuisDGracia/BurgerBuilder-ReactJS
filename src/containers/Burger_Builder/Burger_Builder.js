c41f8894bde07b24115dcf64c65f96edd1b19866
Zuzki <ldegrac071@hotmail.com>
2020-03-03 09:37:18 -0500

    now the modal can be close clicking the backdrop

--- src/containers/Burger_Builder/Burger_Builder.js
+++ src/containers/Burger_Builder/Burger_Builder.js
@@ -80,6 +80,9 @@ class BurgerBuilder extends Component{
 		this.setState({ purchasing: true });
 	}
 
+	purchaseCancelHandler = () => {
+		this.setState({ purchasing: false })
+	}
 
 	render() {
 
@@ -91,7 +94,7 @@ class BurgerBuilder extends Component{
 
 		return (
 			<Aux>
-				<Modal show = { this.state.purchasing }>
+				<Modal show = { this.state.purchasing } modalClosed = { this.purchaseCancelHandler}>
 					<OrderSummary ingredients = { this.state.ingredients }/>
 				</Modal>
 				<Burger ingredients = { this.state.ingredients } />
