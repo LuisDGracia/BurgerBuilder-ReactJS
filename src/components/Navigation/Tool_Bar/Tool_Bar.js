8c954439c93a5bc7a8fe5cd056d0a66f8c81657d
Zuzki <ldegrac071@hotmail.com>
2020-03-04 07:40:06 -0500

    Added the logo of the app

--- src/components/Navigation/Tool_Bar/Tool_Bar.js
+++ src/components/Navigation/Tool_Bar/Tool_Bar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import classes from './Tool_Bar.css'
+import Logo from '../../Logo/Logo'
 
 const tollbar = ( props ) => (
 	<header className = { classes.Toolbar } >
 		<div>MENU</div>
-		<div>LOGO</div>
+		<Logo />
 		<nav>
 			...
 		</nav>
