4e1e8234ce6c068aa29bd203ec6f80a1ec71695d
Zuzki <ldegrac071@hotmail.com>
2020-03-08 09:17:56 -0500

    some thing weird

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,7 +10,7 @@ const withErrorHandler = ( WrappedComponent, axios ) =>{
 			error: null
 		}
 
-		componentWill0Mount() {
+		componentWillMount() {
 			this.reqInterceptor = axios.interceptors.request.use( req => {
 				this.setState( { error: null } )
 				return req
