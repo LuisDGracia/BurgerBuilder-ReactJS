bb402df7bfd6d5b2b24837d0d96baf31ed8c6edd
Zuzki <ldegrac071@hotmail.com>
2020-03-10 12:01:36 -0500

    Change a tag to navlink to change from route

--- src/components/Navigation/Nav_Items/Nav_Item/Nav_Item.js
+++ src/components/Navigation/Nav_Items/Nav_Item/Nav_Item.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 import classes from './Nav_Item.css'
 
 const navItem = ( props ) => (
 	<li className = { classes.NavItem } >
-		<a 
-			href = { props.link } 
-			className = { props.active ? classes.active : null } >{ props.children }</a>
+		<NavLink 
+			to = { props.link }
+			exact = { props.exact }
+			activeClassName = { classes.active } >{ props.children }</NavLink>
 	</li>
 );
 
