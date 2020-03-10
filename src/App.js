8823722318238e3bf72f984f93088360f73a3b76
Zuzki <ldegrac071@hotmail.com>
2020-03-10 11:58:33 -0500

    added a new route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import Layout from './hoc/Layout/Layout'
 import Checkout from './containers/Checkout/Checkout'
+import Orders from './containers/Orders/Orders'
 
 import BurgerBuilder from './containers/Burger_Builder/Burger_Builder'
 
@@ -11,8 +12,9 @@ class App extends Component {
 			<div>
 				<Layout>
 					<Switch>
-						<Route path = '/checkout' component = {Checkout} />
-						<Route path = '/' component = {BurgerBuilder} />
+						<Route path = '/checkout' component = { Checkout } />
+						<Route path = '/orders' component = { Orders } />
+						<Route path = '/' component = { BurgerBuilder } />
 					</Switch>
 				</Layout>
 			</div>
