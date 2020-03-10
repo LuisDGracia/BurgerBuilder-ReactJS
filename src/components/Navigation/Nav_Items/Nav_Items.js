75f641c7785d85d8f228d64c2ce37adc9c17a6e2
Zuzki <ldegrac071@hotmail.com>
2020-03-10 12:00:44 -0500

    changed routes of NavItem so can select to see orders or build a burger

--- src/components/Navigation/Nav_Items/Nav_Items.js
+++ src/components/Navigation/Nav_Items/Nav_Items.js
@@ -6,8 +6,8 @@ import NavItem from './Nav_Item/Nav_Item'
 const navItems = ( props ) => (
 
 	<ul className = { classes.NavItems}>
-		<NavItem link = "/" active >Burger Builder</NavItem>
-		<NavItem link = "/">Checkout</NavItem>
+		<NavItem link = "/" exact>Burger Builder</NavItem>
+		<NavItem link = "/orders">Orders</NavItem>
 	</ul>
 
 );
